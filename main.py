a51d42c4cee31125ddca99bfa60a72efce3d8d24
kriskut08 <73740605+kriskut08@users.noreply.github.com>
2021-03-14 22:26:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,90 +1,107 @@
-"""
-/----------------------------\
-| Tic Tac Toe v4.1 (*^_^*)
-|
-| ▛▀▀▀▀▀▀▀▜
-| ▌ X - - ▐
-| ▌ O O - ▐
-| ▌ - - X ▐
-| ▙▃▃▃▃▃▃▃▟
-|
-\------------------------------/
-"""
-from colorama import *
-init(autoreset=True)
+import tkinter
+from tkinter import Tk
 
+root = Tk()
+root.geometry("550x570")
+root.title("TicTacToe")
+root.iconbitmap('file-49-512.ico')
 
-lap = 0
-charD = [Fore.RED + "X", Fore.BLUE + "O"]
-char = [Fore.RED + "X", Fore.BLUE + "O"]
+charD = ["X", "O"]
+pl = ["X", "O"]
 game = [["-", "-", "-"],
         ["-", "-", "-"],
         ["-", "-", "-"]]
-def PrintBoard():
-    print('\u259B▀▀▀▀▀▀▀\u259C')
-    for i in range(len(game)):
-        for j in range(len(game)):
-            if (j==0):
-                print("▌ " + game[i][j], end=' ')
+
+
+class Button:
+    def __init__(self, row, col, size, char):
+        self.row = row
+        self.col = col
+        self.sze = size
+        self.char = char
+
+        def Check():
+            if (game[0][0] + game[0][1] + game[0][2] == charD[0] * 3): return "r"
+            if (game[0][0] + game[0][1] + game[0][2] == charD[1] * 3): return "b"
+
+            if (game[1][0] + game[1][1] + game[1][2] == charD[0] * 3): return "r"
+            if (game[1][0] + game[1][1] + game[1][2] == charD[1] * 3): return "b"
+
+            if (game[2][0] + game[2][1] + game[2][2] == charD[0] * 3): return "r"
+            if (game[2][0] + game[2][1] + game[2][2] == charD[1] * 3): return "b"
+
+            if (game[0][0] + game[1][0] + game[2][0] == charD[0] * 3): return "r"
+            if (game[0][0] + game[1][0] + game[2][0] == charD[1] * 3): return "b"
+
+            if (game[0][1] + game[1][1] + game[2][1] == charD[0] * 3): return "r"
+            if (game[0][1] + game[1][1] + game[2][1] == charD[1] * 3): return "b"
+
+            if (game[0][2] + game[1][2] + game[2][2] == charD[0] * 3): return "r"
+            if (game[0][2] + game[1][2] + game[2][2] == charD[1] * 3): return "b"
+
+            if (game[0][0] + game[1][1] + game[2][2] == charD[0] * 3): return "r"
+            if (game[0][0] + game[1][1] + game[2][2] == charD[1] * 3): return "b"
+
+            if (game[2][0] + game[1][1] + game[0][2] == charD[0] * 3): return "r"
+            if (game[2][0] + game[1][1] + game[0][2] == charD[1] * 3): return "b"
+
+        def IsDraw():
+            if (str(game[0][0] + game[0][1] + game[0][2] + game[1][0] + game[1][1] + game[1][2] + game[2][0] + game[2][1] + game[2][2]).__contains__("-")):
+                return False
             else:
-                print(game[i][j], end=' ')
-        print("▐")
-    print('\u2599▄▄▄▄▄▄▄\u259F')
+                return True
 
-def Check():
-    if (game[0][0] + game[0][1] + game[0][2] == charD[0]*3): return "r"
-    if (game[0][0] + game[0][1] + game[0][2] == charD[1]*3): return "b"
+        def k():
+            root.quit()
 
-    if (game[1][0] + game[1][1] + game[1][2] == charD[0]*3): return "r"
-    if (game[1][0] + game[1][1] + game[1][2] == charD[1]*3): return "b"
+        def Play():
+            if (IsDraw() == False):
+                if (game[row][col].__contains__("-")):
+                    if (pl[0] == "X"):
+                        name.config(text=pl[0], bg='red', fg='white')
+                    if (pl[0] == "O"):
+                        name.config(text=pl[0], bg='blue', fg='white')
+                    game[row][col] = pl[0]
+                    # print(game)
+                    pl[0], pl[1] = pl[1], pl[0]
+                    if (Check() == "r"):
+                        k()
+                        win = Tk()
+                        win.geometry("200x200")
+                        win.title("TicTacToe")
+                        win.iconbitmap('file-49-512.ico')
+                        txt = tkinter.Label(win, text="A piros nyert!")
+                        txt.pack()
+                        win.mainloop()
+                    if (Check() == "b"):
+                        k()
+                        win = Tk()
+                        win.geometry("200x200")
+                        win.title("TicTacToe")
+                        win.iconbitmap('file-49-512.ico')
+                        txt = tkinter.Label(win, text="A kék nyert")
+                        txt.pack()
+                        win.mainloop()
 
-    if (game[2][0] + game[2][1] + game[2][2] == charD[0]*3): return "r"
-    if (game[2][0] + game[2][1] + game[2][2] == charD[1]*3): return "b"
+                    if (IsDraw() == True and Check() != "r" and Check() != "b"):
+                        root.quit()
+                        win = Tk()
+                        win.geometry("200x200")
+                        win.title("TicTacToe")
+                        txt = tkinter.Label(win, text="Döntetlen!")
+                        txt.pack()
+                        win.mainloop()
 
-    if (game[0][0] + game[1][0] + game[2][0] == charD[0]*3): return "r"
-    if (game[0][0] + game[1][0] + game[2][0] == charD[1]*3): return "b"
+        name = tkinter.Button(root, text=char, width=size, height=int(size / 2), command=Play)
+        name.grid(row=row, column=col)
 
-    if (game[0][1] + game[1][1] + game[2][1] == charD[0]*3): return "r"
-    if (game[0][1] + game[1][1] + game[2][1] == charD[1]*3): return "b"
 
-    if (game[0][2] + game[1][2] + game[2][2] == charD[0]*3): return "r"
-    if (game[0][2] + game[1][2] + game[2][2] == charD[1]*3): return "b"
+def placeBtn():
+    for i in range(3):
+        for j in range(3):
+            btn = Button(i, j, 25, str(game[i][j]))
 
-    if (game[0][0] + game[1][1] + game[2][2] == charD[0]*3): return "r"
-    if (game[0][0] + game[1][1] + game[2][2] == charD[1]*3): return "b"
 
-    if (game[2][0] + game[1][1] + game[0][2] == charD[0]*3): return "r"
-    if (game[2][0] + game[1][1] + game[0][2] == charD[1]*3): return "b"
+placeBtn()
 
-def IsDraw():
-    if (str(game[0][0] + game[0][1] + game[0][2]+ game[1][0] + game[1][1] + game[1][2] + game[2][0] + game[2][1] + game[2][2]).__contains__("-")):
-        return False
-    else:
-        return True
-
-def Game():
-    while True:
-        if (not IsDraw()):
-            choice = input("hova tegyem?")
-            if (choice.__contains__(":")):
-                x = int(choice[0]) - 1
-                y = int(choice[2]) - 1
-            if (game[x][y] == "-"):
-                game[x][y] = char[0]
-                temp = char[0]
-                char[0] = char[1]
-                char[1] = temp
-                if (Check()=="b"):
-                    print(Fore.BLUE +"A kék nyert!")
-                    PrintBoard()
-                    break
-                elif (Check()=="r"):
-                    print(Fore.RED + "A piros nyert!")
-                    PrintBoard()
-                    break
-                else:
-                    PrintBoard()
-        else:
-            print("Döntetlen!")
-            break
-Game()
+root.mainloop()
